2b8a374e93e1b9f56e3aaf1431b759009c0e563b
Nikhil p rao <nikhilprao9066@gmail.com>
2025-01-18 17:44:15 -0500

    added personal pic

--- src/images.ts
+++ src/images.ts
@@ -1,5 +1,5 @@
 export const images = {
-  profile: "./images/cvs.JPEG",
+  profile: "https://github.com/nikhil9066/portfolio/blob/main/images/IMG_6321_2_processed.jpg",
   project1: "./images/project 1.jpg",
   project2: "./images/cvs.JPEG",
   resume: "./images/Nikhil_PremachandraRao_Resume.pdf"  // Replace with your actual resume URL
