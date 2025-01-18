3df3d27d6bc903bec716a8533e93c43bd3e0f80e
Nikhil p rao <nikhilprao9066@gmail.com>
2025-01-18 17:27:08 -0500

    added pics and others

--- src/images.ts
+++ src/images.ts
@@ -1,6 +1,6 @@
 export const images = {
-  profile: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?auto=format&fit=crop&w=800&q=80",
-  project1: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?auto=format&fit=crop&w=1200&q=80",
-  project2: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?auto=format&fit=crop&w=1200&q=80",
-  resume: "images/Nikhil_PremachandraRao_Resume.pdf"  // Replace with your actual resume URL
+  profile: "./images/cvs.JPEG",
+  project1: "./images/project 1.jpg",
+  project2: "./images/cvs.JPEG",
+  resume: "./images/Nikhil_PremachandraRao_Resume.pdf"  // Replace with your actual resume URL
 } as const;
